c513523957e26d2caf4886397b012aa5bc106bad
github username <gregm9999@gmail.com>
2019-06-13 14:53:45 -0700

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,110 @@
 const fi = (function() {
   return {
     libraryMethod: function() {
-      return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
+      return "Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0";
     },
 
-    each: function() {
-
+    each: function(collection, callback) {
+      const newCollection = Array.isArray(collection)
+        ? collection.slice()
+        : Object.values(collection);
+      for (let i = 0; i < newCollection.length; i++) {
+        callback(newCollection[i]);
+      }
+      return collection;
     },
 
-    map: function() {
-
+    map: function(collection, callback) {
+      let newCollection = [];
+      if (!Array.isArray(collection)) {
+        collection = Object.values(collection);
+      }
+      for (let i = 0; i < collection.length; i++) {
+        newCollection.push(callback(collection[i]));
+      }
+      return newCollection;
     },
 
-    reduce: function() {
-
+    reduce: function(collection, callback, acc) {
+      let newCollection = collection.slice(0);
+      if (!acc) {
+        acc = collection[0];
+        newCollection = collection.slice(1);
+      }
+      for (let i = 0; i < newCollection.length; i++) {
+        acc = callback(acc, newCollection[i], collection);
+      }
+      return acc;
     },
 
-    functions: function() {
-
+    find: function(collection, predicate) {
+      const newCollection = Array.isArray(collection)
+        ? collection.slice(0)
+        : Object.values(collection);
+      for (let i = 0; i < newCollection.length; i++) {
+        if (predicate(newCollection[i])) {
+          return collection[i];
+        }
+      }
+      return undefined;
     },
 
+    filter: function(collection, predicate) {
+      const newCollection = Array.isArray(collection)
+        ? collection.slice(0)
+        : Object.values(collection);
+      let retAr = [];
+      for (let i = 0; i < newCollection.length; i++) {
+        if (predicate(newCollection[i])) {
+          retAr.push(newCollection[i]);
+        }
+      }
+      return retAr;
+    },
 
-  }
-})()
+    size: function(collection) {
+      const newCollection = Array.isArray(collection)
+        ? collection.slice(0)
+        : Object.values(collection);
+      return newCollection.length;
+    },
 
-fi.libraryMethod()
+    first: function(collection, elements = 1) {
+      const newCollection = Array.isArray(collection)
+        ? collection.slice(0)
+        : Object.values(collection);
+      let retAr = [];
+      for (let i = 0; i < elements; i++) {
+        retAr.push(newCollection[i]);
+      }
+      return elements === 1 ? retAr.shift() : retAr;
+    },
+
+    last: function(collection, elements = 0) {
+      const newCollection = Array.isArray(collection)
+        ? collection.slice(0)
+        : Object.values(collection);
+      let retAr = [];
+      for (let i = 0; i < newCollection.length; i++) {
+        retAr.push(newCollection[i]);
+      }
+      return elements
+        ? newCollection.slice(
+            newCollection.length - elements,
+            newCollection.length
+          )
+        : newCollection.pop();
+    },
+
+    compact: function(array) {
+      let newAr = [];
+      for (let i = 0; i < array.length; i++) {
+        array[i] ? newAr.push(array[i]) : null;
+      }
+      return newAr;
+    },
+    functions: function() {}
+  };
+})();
+
+fi.libraryMethod();
